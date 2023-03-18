07b1b43125794ed2452f6eabaee5ba09e1f8e2f2
Fitsum <fitbez@gmail.com>
2023-03-17 20:22:01 -0400

    added a paragraph

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ function App() {
   return (
     <div className='App'>
       <h1>Hello from the new react application</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente atque
+        pariatur accusantium corporis praesentium. Nobis nulla distinctio animi
+        maiores asperiores dolorum atque earum placeat quos eos? Omnis laborum
+        explicabo consequatur ipsam voluptatibus iure perspiciatis rerum, fuga,
+        perferendis aspernatur laudantium officia adipisci velit aperiam
+        voluptatem cumque ea nobis non inventore debitis? Adipisci assumenda
+        tempora eaque, corporis dignissimos iure praesentium repellendus ut
+        dolor cupiditate ea quia dolorum qui dicta reiciendis quae officiis sunt
+        culpa fugit atque? Magnam, sequi! Impedit nam qui, aliquam expedita
+        dolores, eum esse eaque quam blanditiis non vero magni ex dolorum
+        laudantium fugiat! Explicabo beatae neque eligendi alias maiores!
+      </p>
     </div>
   );
 }
